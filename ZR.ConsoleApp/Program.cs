df930f030b947c6c7ef9ea237fc71f12dc1a917d
程序猴子 <ms363255751@outlook.com>
2025-11-04 10:54:19 +0800

    1

--- ZR.ConsoleApp/Program.cs
+++ ZR.ConsoleApp/Program.cs
@@ -1,8 +1,6 @@
 ﻿using Infrastructure.Extensions;
 using MiniExcelLibs;
 using Models;
-
-//using Models;
 using NPOI.SS.UserModel;
 using OfficeOpenXml;
 using SqlSugar;
@@ -10,6 +8,9 @@ using ZR.Common;
 using ZR.Common.ExcelHelper;
 using ZR.Infrastructure.Extensions;
 
+/// 1.编辑excel文件，删除指定日期之前的数据
+/// 2.查询最新的公式计算结果
+/// 3.批量导入excel文件，生成对应的数据库表
 Test();
 void Test()
 {
