3b0d5cb18f7730f4939ed7e007f25f29c13083cc
MENGKAI\ms363 <363255751@qq.com>
2026-01-21 11:45:12 +0800

    console 加入 rule demo

---
+++ ZR.ConsoleApp/RulesEngineDemo.cs
@@ -0,0 +1,40 @@
+﻿using RulesEngine.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using RulesEngine;
+using System.Threading.Tasks; // 确保引用了正确的命名空间
+namespace ZR.ConsoleApp
+{
+    public class RulesEngineDemo
+    {
+        public async static Task Run()
+        {
+            Console.WriteLine("RulesEngineDemo running...");
+            // Implementation of the rules engine demo goes here.
+
+            // 修复 CS0103: 定义 workflow 变量
+            var workflow = new Workflow[] { 
+                new Workflow
+                {
+                    WorkflowName = "DemoWorkflow",
+                    Rules = new List<Rule>
+                    {
+                        new Rule
+                        {
+                            RuleName = "AlwaysTrue",
+                            Expression = "true"
+                        }
+                    }
+                }
+            };
+        
+            // 修复 CS0118: 明确指定类型为 RulesEngine.RulesEngine
+            var rulesEngine = new RulesEngine.RulesEngine(workflow);
+
+
+           await rulesEngine.ExecuteActionWorkflowAsync("DemoWorkflow", "AlwaysTrue", null);
+
+        }
+    }
+}
