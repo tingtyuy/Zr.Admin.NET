9d79d96ed0d79eacd3ad94c9f0ac88da11d3b1b7
MENGKAI\ms363 <363255751@qq.com>
2026-01-29 09:53:44 +0800

    添加单元测试

---
+++ ZR.NUnit/Masuit_Tools_UnitTest.cs
@@ -0,0 +1,21 @@
+﻿using Masuit.Tools.DateTimeExt;
+using System.Data;
+
+namespace ZR.NUnit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var timeStamp = DateTime.Now.GetTotalSeconds();
+            Console.WriteLine(timeStamp);
+            Assert.Pass();
+        }
+    }
+}
